3178e3d5dbd789ca06a9ee345fbbe91f1c021547
rukavicyn-mn <rukavicyn-mn@it-resource.team>
2025-09-30 16:38:23 +0500

    start

---
+++ src/main/java/com/example/interview/work_project_spring/WorkProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.interview.work_project_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkProjectSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/interview/work_project_spring/WorkProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interview.work_project_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
